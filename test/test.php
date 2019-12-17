2876aff6514031cfe33613f0f13d3f40f841eb6e
Chaman Narved <chamannarved@gmail.com>
2019-12-17 16:09:07 +0530

    debuggin

--- test/test.php
+++ test/test.php
@@ -1,19 +1,18 @@
 <?php
-    {   //Secure Connection Script
-        include(../Connect_DB/alphacrm_DB.php);
-        $dbSuccess = false;
-        $dbConnected = mysql_connect($db['hostname'],$db['username'],$db[password]);
+    //Secure Connection Script
+    include("../Connect_DB/alphacrm_DB.php");
+    $dbSuccess = false;
+    $dbConnected = mysql_connect($db['hostname'],$db['username'],$db['password']);
 
-        if($dbConnected){
-            $dbSelected = mysql_select_db($db[database],$dbConnected);
-            if ($dbSelected){
-                $dbSuccess = true;
-            }
+    if($dbConnected){
+        $dbSelected = mysql_select_db($db[database],$dbConnected);
+        if ($dbSelected){
+            $dbSuccess = true;
         }
-        // END Secure Connection Script 
     }
+    // END Secure Connection Script 
 
     if($dbSuccess){
-        echo("Success")
+        echo("Success");
     }
 ?>
