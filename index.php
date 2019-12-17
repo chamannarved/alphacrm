334ecc90fc751a034388a88b17ea2fdd458704ce
Chaman Narved <chamannarved@gmail.com>
2019-12-17 14:40:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-    echo("Hello World </br>");
-    echo("My first php Project");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My PHP Project</title>
+</head>
+<body>
+
+    <?php
+
+        echo("
+            <h1>Contect Mangment System</h1>
+        ");
+
+        echo('
+            <a href="createCompany.php">Create New Company</a>
+        ')
+
+    ?>
+
+</body>
+</html>
